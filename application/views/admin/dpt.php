c5a3e6ff491b044b9be2dc9f75bdae8fe1b9cdf2
Yahdi almukaram <yahdialmukaram03@gmail.com>
2020-11-23 15:33:46 +0700

    update pdf

--- application/views/admin/dpt.php
+++ application/views/admin/dpt.php
@@ -22,13 +22,21 @@
 </head>
 <body>
 
-                <h3 style="text-align: center;">DATA PEMILIH TETAP (DPT) KECAMATAN BATIPUH SELATAN</h3>
-                <h3 style="text-align: center;"><u>KABUPATEN TANAH DATAR</u></h3>
-                
-                <br>
-                <br>
-                <h5>Tahun : 2020 </h5>
+  
+  <!-- <img style=" width: 80px;height: 80px;" src="uploads/original_image/lambang.png" align="left"> -->
 
+
+<img src="uploads/original_image/lambang.png" style="float:left; width: 90px; height: 90px;"><h3 style="text-align: center;"><u>DATA PEMILIH TETAP (DPT) KECAMATAN BATIPUH SELATAN</u> <br><br><u>KABUPATEN TANAH DATAR</u></h3>
+  
+
+  <!-- <p style="text-align: center;">DATA PEMILIH TETAP (DPT) KECAMATAN BATIPUH SELATAN</p>
+  <p style="text-align: center;"><u>KABUPATEN TANAH DATAR</u></p> -->
+                
+                
+              
+                <br>
+                <h5>Tanggal Print : <?= date('d-m-Y');?> </h5>
+              
                     <table>
                       <thead>
                       <tr>
@@ -39,8 +47,7 @@
                           <th>Tempat lahir</th>
                           <th>Tanggal lahir</th>
                           <th>Jenis kelamin</th>
-                          <!-- <th>Foto ktp<mphp
-                            /th> -->
+                          <th>Foto ktp</th>
                           <th>Status</th>
                           <th>Telapon</th>
                           <th>Agama</th>
@@ -59,16 +66,16 @@
                           <td><?=$value['tempat_lahir'];?></td>
                           <td><?=$value['tanggal_lahir'];?></td>
                           <td><?=$value['jenis_kelamin'];?></td>
-                          <!-- <td><button type="button" class="label label-primary" onclick="show_photo('<?=$value['id_masyarakat']?>')"><i class= 'fa fa-check'> Klik foto</i> </button></td> -->
-													<td>
-														<?php if ($value['status']==0):?>
-													<a href="#" class="label label-danger" onclick="verifikasi(<?=$value['id_masyarakat']?>);"><i class="fa fa-ban"></i> Belum Verifikasi</a>
-													<?php elseif ($value['status']==1):?>
-													<a href="#" class="label label-success" onclick="cancel_verifikasi(<?=$value['id_masyarakat']?>)"><i class="fa fa-check"></i> Terverifikasi</a>
-													<?php elseif ($value['status']==2):?>
-													<a href="#" class="label label-warning"><i class="fa fa-check"></i> Telah Memilih</a>
-													<?php endif;?>
-													</td>
+                          <td><button type="button" class="label label-primary" onclick="show_photo('<?=$value['id_masyarakat']?>')"><i class= 'fa fa-check'> Klik foto</i> </button></td>
+						        	<td>
+						        		<?php if ($value['status']==0):?>
+						        	<a href="#" class="label label-danger" onclick="verifikasi(<?=$value['id_masyarakat']?>);"><i class="fa fa-ban"></i> Belum Verifikasi</a>
+						        	<?php elseif ($value['status']==1):?>
+						        	<a href="#" class="label label-success" onclick="cancel_verifikasi(<?=$value['id_masyarakat']?>)"><i class="fa fa-check"></i> Terverifikasi</a>
+						        	<?php elseif ($value['status']==2):?>
+						        	<a href="#" class="label label-warning"><i class="fa fa-check"></i> Telah Memilih</a>
+						        	<?php endif;?>
+						        	</td>
                           <td><?=$value['no_hp'];?></td>
                           <td><?=$value['agama'];?></td>
                           <td><?=$value['status_perkawinan'];?></td>
